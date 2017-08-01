4517955be4fd411c677540ae503ae06f305c3c0c
yordank <yordankp@gmail.com>
2017-08-01 20:16:42 +0300

    Kamikaze and Satellite classes

---
+++ TheTieSilincer/Models/Satellite/Satellite.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TheTieSilincer.Models;
+using TheTieSilincer.Models.Ships;
+
+public class Satellite
+{
+
+    public Satellite()
+    {
+        this.position = new Position(0,0);
+    }
+    public event EventHandler SendData2;
+    public void StartSendingData()
+    {
+        this.SendData2(this, EventArgs.Empty);
+    }
+
+    private Position position;
+
+    public Position Position
+    {
+        get
+        {
+            return position;
+        }
+
+        set
+        {
+            position = value;
+        }
+    }
+
+    public void ReceiveDataByPlayer(PlayerShip playerShip)
+    {
+        playerShip.SendData -= PlayerShipSendCoords;
+
+        playerShip.SendData += PlayerShipSendCoords;
+    }
+
+    public void PlayerShipSendCoords(object sender, EventArgs e)
+    {
+        this.position = ((PlayerShip)sender).Position;
+
+       // this.StartSendingData();
+    }
+}
+
---
+++ TheTieSilincer/Models/Ships/KamikazeShip.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using TheTieSilincer.Models.Weapons;
+
+namespace TheTieSilincer.Models.Ships
+{
+    public class KamikazeShip : EnemyShip
+    {
+        //   \^/
+        //    V
+
+
+
+
+        public void ListenPlayerShipCoords(Satellite satellite)
+        {
+            satellite.SendData2 -= PlayerShipSendCoords;
+            satellite.SendData2 += PlayerShipSendCoords;
+        }
+        //public void ListenPlayerShipCoords(PlayerShip satellite)
+        //{
+        //    satellite.SendData -= PlayerShipSendCoords;
+        //    satellite.SendData += PlayerShipSendCoords;
+        //}
+        public  void PlayerShipSendCoords(object sender, EventArgs e)
+        {
+            var position = ((Satellite)sender).Position;
+
+            if (position.Y != this.Position.Y)
+            {
+
+                this.pos = position;
+
+            }
+        }
+
+        private Position pos = null;
+        
+        private double movementTime = 0;
+
+         
+
+        public override void ClearShip()
+        {
+            if (this.PreviousPosition != null)
+            {
+
+                Console.SetCursorPosition(this.PreviousPosition.Y, this.PreviousPosition.X);
+                Console.Write(@"    ");
+                Console.SetCursorPosition(this.PreviousPosition.Y + 1, this.PreviousPosition.X + 1);
+                Console.Write(@" ");
+
+
+
+                Console.SetCursorPosition(this.Position.Y, this.Position.X);
+                Console.Write(@"    ");
+                Console.SetCursorPosition(this.Position.Y + 1, this.Position.X + 1);
+                Console.Write(@" ");
+
+                
+
+            }
+        }
+
+        public override void DrawShip()
+        {
+            Console.SetCursorPosition(this.Position.Y, this.Position.X);
+            Console.WriteLine(@"\^/");
+            Console.SetCursorPosition(this.Position.Y + 1, this.Position.X + 1);
+            Console.WriteLine("V");
+
+            
+
+             
+        }
+
+        public override void UpdateShip()
+        {
+             
+
+            if (movementTime % 2 == 0)
+            {
+                this.PreviousPosition = new Position(this.Position.X, this.Position.Y);
+
+                this.Position.X++;
+
+                if (this.pos != null)
+                {
+                    if (this.pos.Y < this.Position.Y)
+                    {
+                        this.Position.Y--;
+                    }
+
+                    if (this.pos.Y > this.Position.Y)
+                    {
+                        this.Position.Y++;
+                    }
+                }
+
+
+
+
+            }
+
+            movementTime += 0.5;
+        }
+
+        public override bool InBounds(Position nextDirection)
+        {
+            if (Position.X == Console.WindowHeight - 2)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override void GenerateBullets()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
