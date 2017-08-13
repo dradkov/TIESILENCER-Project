afb07c528cce07ff6d1cbaadc0815e705ce87b4a
Valentin <valentin_github@abv.bg>
2017-08-13 18:49:15 +0300

    TestAgain

---
+++ TheTieSilincer.Models/Models/PlayerDb.cs
@@ -0,0 +1,9 @@
+﻿namespace TheTieSilincer.Models.Models
+{
+    public class PlayerDb
+    {
+        public int Id { get; private set; }
+        public string FullName { get; private set; }
+        public string Password { get; private set; }
+    }
+}
